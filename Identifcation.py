58dafca0f543f17b2ccd8a737c2c3d8b12aabb78
Your Name <jessyjcw@gmail.com>
2023-12-04 21:38:42 +0700

    update Identification

--- Identifcation.py
+++ Identifcation.py
@@ -34,9 +34,9 @@ def main():
     known_faces, known_names = load_known_faces(data_folder)
 
     cap = cv2.VideoCapture(0)
-    face_matched = False
+    stop_on_no_match = True  # Set this to False if you want continuous scanning without stopping
 
-    while not face_matched:
+    while True:
         ret, frame = cap.read()
         if not ret:
             break
@@ -45,17 +45,28 @@ def main():
         face_locations = face_recognition.face_locations(rgb_frame)
         face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
+        face_matched = False
         for face_encoding in face_encodings:
-            distances = face_recognition.face_distance(known_faces, face_encoding)
-            if len(distances) > 0:
-                best_match_index = np.argmin(distances)
-                if distances[best_match_index] < 0.6:  # You can adjust the threshold
-                    name = known_names[best_match_index]
+            if len(face_locations) == 0:
+                print("No faces detected in the frame.")
+                continue
 
-                    save_attendance(name)
-                    print(f"Attendance recorded for {name}")
-                    face_matched = True
-                    break
+            distances = face_recognition.face_distance(known_faces, face_encoding)
+            if len(distances) > 0 and min(distances) < 0.6:  # Threshold for a match
+                best_match_index = np.argmin(distances)
+                name = known_names[best_match_index]
+                save_attendance(name)
+                print(f"Attendance recorded for {name}")
+                face_matched = True
+                break
+
+        if face_matched:
+            break
+
+        if not face_matched and face_encodings:
+            print("No match found.")  # Handle no match case
+            if stop_on_no_match:
+                break
 
         cv2.imshow("Video", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -63,6 +74,7 @@ def main():
 
     cap.release()
     cv2.destroyAllWindows()
+
 
 
 if __name__ == "__main__":
